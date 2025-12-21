35ad5490067d896b217755ca45245f30b5af8061
Atakan Akın <atakan.akin@metu.edu.tr>
2025-12-21 14:41:01 +0300

    chore: replace greeting with YouTube chapterization pipeline execution in main function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from service.run import run_youtube_chapterize_pipeline
+
+
 def main():
-    print("Hello from chapterize!")
+    run_youtube_chapterize_pipeline(
+        youtube_url="https://www.youtube.com/watch?v=6jKGclan4LE"  # Example URL :D
+    )
 
 
 if __name__ == "__main__":
